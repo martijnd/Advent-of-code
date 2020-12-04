55de5ff021317402d4ca68cfa4dc4ed4f910c76b
Martijn Dorsman <dorsman@mytaxflow.dev>
2020-12-04 15:47:21 +0100

    Cleanup code

--- 2020/Day4/index.ts
+++ 2020/Day4/index.ts
@@ -1,64 +1,55 @@
 export function part1 (input: string) {
-  const data = formatData(input).reduce((acc, passport) => {
-    const keys = Object.fromEntries(passport)
-    if (Object.keys(keys).length === 8) {
+  return getPassports(input).reduce((acc, passport) => {
+    const passwordObj = Object.fromEntries(passport)
+    if (Object.keys(passwordObj).length === 8) {
       return acc + 1
     }
 
-    if (!Object.keys(keys).includes('cid') &&
-        Object.keys(keys).length === 7) {
+    if (!Object.keys(passwordObj).includes('cid') &&
+        Object.keys(passwordObj).length === 7) {
       return acc + 1
     }
 
     return acc
   }, 0)
-  return data
 }
 
 export function part2 (input: string) {
+  const passports = getPassports(input)
+
   const validationRules: {[key: string]: (value: string) => boolean} = {
     byr: (value: string) => +value >= 1920 && +value <= 2002,
     iyr: (value: string) => +value >= 2010 && +value <= 2020,
     eyr: (value: string) => +value >= 2020 && +value <= 2030,
     hgt: (value: string) => {
       if (value.endsWith('in')) {
-        return +value.split('i')[0] >= 59 && +value.split('i')[0] <= 76
+        return +value.split('in')[0] >= 59 && +value.split('in')[0] <= 76
       } else {
-        return +value.split('c')[0] >= 150 && +value.split('c')[0] <= 193
+        return +value.split('cm')[0] >= 150 && +value.split('cm')[0] <= 193
       }
     },
-    hcl: (value: string) => /#[0-9a-f]{6}/.test(value),
+    hcl: (value: string) => /^#[0-9a-f]{6}$/.test(value),
     ecl: (value: string) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value),
     pid: (value: string) => value.length === 9 && !isNaN(parseInt(value)),
     cid: (value: string) => true
   }
 
-  const passports = formatData(input)
-
   return passports.reduce((acc, passport) => {
-    const entriesAmountValid = passport.length === 8 ||
-      (passport.length === 7 && !Object.keys(Object.fromEntries(passport)).includes('cid'))
-
-    const entriesValid = passport.reduce((acc, passportEntry) => {
-      // console.log(passportEntry)
-
-      if (validationRules[passportEntry[0]](passportEntry[1])) {
-        return acc + 1
-      }
-      return acc
+    const validEntries = passport.reduce((acc, passportEntry) => {
+      const [key, value] = passportEntry
+      return validationRules[key](value) ? acc + 1 : acc
     }, 0)
 
-    if (!Object.keys(Object.fromEntries(passport)).includes('cid')) {
-      return entriesValid === 7 && entriesAmountValid ? acc + 1 : acc
-    } else {
-      return entriesValid === 8 && entriesAmountValid ? acc + 1 : acc
-    }
-  }, 0)
+    const neededValidEntries = (!Object.keys(Object.fromEntries(passport)).includes('cid') ? 7 : 8)
+    const numberOfValidEntries = validEntries === neededValidEntries
+    const entriesAllValid = (passport.length === 8 ||
+      (passport.length === 7 && !Object.keys(Object.fromEntries(passport)).includes('cid')))
 
-  // console.log(formatData(input))
+    return numberOfValidEntries && entriesAllValid ? acc + 1 : acc
+  }, 0)
 }
 
-const formatData = (input: string) =>
+const getPassports = (input: string) =>
   input.split('\n')
     .map(x => x === '' ? '_' : x)
     .join(' ')
