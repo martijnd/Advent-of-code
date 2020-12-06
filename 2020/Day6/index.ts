f11e465ed3ffaa48a0eb6e0bd7310e1e1add72f4
Martijn Dorsman <martijn.dorsman@gmail.com>
2020-12-06 13:56:09 +0100

    Use spread syntax to split string

--- 2020/Day6/index.ts
+++ 2020/Day6/index.ts
@@ -1,14 +1,14 @@
 export function part1 (input: string) {
   return formatInput(input).map(person => person.replace(/\n/g, ''))
     .reduce((acc, group) =>
-      acc + group.split('').reduce((acc: string[], char) =>
+      acc + [...group].reduce((acc: string[], char) =>
         acc.includes(char) ? acc : [...acc, char], []).length, 0)
 }
 
 export function part2 (input: string) {
   return formatInput(input).reduce((acc, group) => {
     const persons = group.split(/\n/)
-    return acc + persons[0].split('').reduce((acc, char) =>
+    return acc + [...persons[0]].reduce((acc, char) =>
       persons.every(person => person.includes(char)) ? acc + 1 : acc, 0)
   }, 0)
 }
