3c9edbacbcd410649d4b5b660c31f5735fa9dc11
Martijn Dorsman <martijn.dorsman@gmail.com>
2020-12-06 13:06:24 +0100

    Solve day 6

---
+++ 2020/Day6/index.test.ts
@@ -0,0 +1,29 @@
+import input from './input'
+import { part1, part2 } from './index'
+
+const exampleData = `abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`
+
+test('2020 - Day 6: part 1', () => {
+  expect(part1(exampleData)).toBe(11)
+  expect(part1(input)).toBe(6430)
+})
+
+test('2020 - Day 6: part 2', () => {
+  expect(part2(exampleData)).toBe(6)
+  console.log(part2(input))
+  // expect(part1(input)).toBe(6430)
+})
