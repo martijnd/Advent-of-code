ec9685b3176cdf1fc68b1011c1db11e371c3883e
Martijn Dorsman <dorsman@mytaxflow.dev>
2020-12-10 09:53:36 +0100

    Solve day 10 part 1

--- 2020/Day10/index.test.ts
+++ 2020/Day10/index.test.ts
@@ -3,6 +3,18 @@ import { part1, part2 } from './index'
 
 const data = getInput(__dirname)
 
+const exampleData2 = `16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`
+
 const exampleData = `28
 33
 18
@@ -36,8 +48,9 @@ const exampleData = `28
 3`
 
 test('2020 - Day 10: part 1', () => {
+  expect(part1(exampleData2)).toBe(7 * 5)
   expect(part1(exampleData)).toBe(22 * 10)
-  expect(part1(data)).toBe(0)
+  expect(part1(data)).toBe(2775)
 })
 
 // test('2020 - Day 10: part 2', () => {
