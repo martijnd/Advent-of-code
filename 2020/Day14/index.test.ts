e15a7f63f2bb2be060820c4d6c833eba55c77880
Martijn Dorsman <dorsman@mytaxflow.dev>
2020-12-14 17:44:34 +0100

    Add day 14

---
+++ 2020/Day14/index.test.ts
@@ -0,0 +1,16 @@
+import { getInput } from '../../utils/getInput'
+import { part1, part2 } from './index'
+
+const data = getInput(__dirname)
+
+const exampleData = ``
+
+test('2020 - Day 14: part 1', () => {
+  expect(part1(exampleData)).toBe(undefined)
+  // expect(part1(data)).toBe(undefined)
+})
+
+// test('2020 - Day 14: part 2', () => {
+//   expect(part2(exampleData)).toBe(undefined)
+//   // expect(part2(data)).toBe(undefined)
+// })
---
+++ 2020/Day14/index.ts
@@ -0,0 +1,7 @@
+export function part1 (input: string) {
+
+}
+
+export function part2 (input: string) {
+
+}
