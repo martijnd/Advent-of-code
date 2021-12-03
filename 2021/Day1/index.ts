5eaa7cbcd40b9f2ecc460556fc291b0db533fdb1
Martijn Dorsman <dorsman@mytaxflow.dev>
2021-12-03 14:14:07 +0100

    ESLint

--- 2021/Day1/index.ts
+++ 2021/Day1/index.ts
@@ -1,27 +1,27 @@
-export function part1(input: string): number {
-  let count = 0;
-  input.split("\n").map(Number).forEach((num, index, data) => {
+export function part1 (input: string): number {
+  let count = 0
+  input.split('\n').map(Number).forEach((num, index, data) => {
     if (index > 0) {
       if (num > data[index - 1]) {
-        count++;
+        count++
       }
     }
-  });
+  })
 
-  return count;
+  return count
 }
 
-export function part2(input: string): number {
-  let count = 0;
-  input.split("\n").map(Number).forEach((num, index, data) => {
+export function part2 (input: string): number {
+  let count = 0
+  input.split('\n').map(Number).forEach((num, index, data) => {
     if (index > 2) {
-      const currTotal = num + data[index - 1] + data[index - 2];
-      const prevTotal = data[index - 1] + data[index - 2] + data[index - 3];
+      const currTotal = num + data[index - 1] + data[index - 2]
+      const prevTotal = data[index - 1] + data[index - 2] + data[index - 3]
       if (currTotal > prevTotal) {
-        count++;
+        count++
       }
     }
-  });
+  })
 
-  return count;
+  return count
 }
