3247d7838a311b26d943d0ecb71874af79875ef5
Martijn Dorsman <dorsman@mytaxflow.dev>
2021-12-07 09:52:36 +0100

    wip

---
+++ 2021/Day7/index.test.ts
@@ -0,0 +1,16 @@
+import { getInput } from '../../utils/getInput'
+import { part1, part2 } from './index'
+
+const data = getInput(__dirname)
+
+const exampleData = `16,1,2,0,4,2,7,1,2,14`
+
+test('2021 - Day 7: part 1', () => {
+  expect(part1(exampleData)).toBe(37)
+  // expect(part1(data)).toBe(undefined)
+})
+
+// test('2021 - Day 7: part 2', () => {
+//   expect(part2(exampleData)).toBe(undefined)
+//   // expect(part2(data)).toBe(undefined)
+// })
---
+++ 2021/Day7/index.ts
@@ -0,0 +1,12 @@
+export function part1 (input: string) {
+  const crabSubmarines = input.split(',').map(Number);
+  
+  crabSubmarines.forEach(number => {
+    let sum = 0;
+    
+  })
+}
+
+export function part2 (input: string) {
+
+}
