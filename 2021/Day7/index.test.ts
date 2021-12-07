abf5985fea50fae4d8e3bc8104b146f41bf29bf1
Martijn Dorsman <dorsman@mytaxflow.dev>
2021-12-07 12:33:51 +0100

    Solve day 7 part 1

--- 2021/Day7/index.test.ts
+++ 2021/Day7/index.test.ts
@@ -7,7 +7,7 @@ const exampleData = `16,1,2,0,4,2,7,1,2,14`
 
 test('2021 - Day 7: part 1', () => {
   expect(part1(exampleData)).toBe(37)
-  // expect(part1(data)).toBe(undefined)
+  expect(part1(data)).toBe(347449)
 })
 
 // test('2021 - Day 7: part 2', () => {
--- 2021/Day7/index.ts
+++ 2021/Day7/index.ts
@@ -1,10 +1,18 @@
 export function part1 (input: string) {
   const crabSubmarines = input.split(',').map(Number);
   
-  crabSubmarines.forEach(number => {
-    let sum = 0;
-    
-  })
+  let max = Infinity;
+  
+  crabSubmarines.forEach((number, idx) => {
+    const crabSubmarinesFiltered = crabSubmarines.filter((_, idx2) => idx2 !== idx);
+    const sum = crabSubmarinesFiltered.reduce((acc, curr) => acc + Math.abs(number - curr), 0)
+    if (sum < max) {
+      max = sum;
+    } 
+
+  }, Infinity)
+  
+  return max;
 }
 
 export function part2 (input: string) {
