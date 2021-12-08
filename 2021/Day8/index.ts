6ca0e157084d2d8b70a844914a10d87a74ec1fbe
Martijn Dorsman <dorsman@mytaxflow.dev>
2021-12-08 17:39:25 +0100

    wip

--- 2021/Day8/index.ts
+++ 2021/Day8/index.ts
@@ -1,6 +1,6 @@
 export function part1(input: string) {
   const values = input.split('\n');
-  
+
   return values.reduce((total, value) => {
     const digits = value.split(' | ')[1].split(' ');
 
@@ -17,4 +17,22 @@ export function part1(input: string) {
   }, 0);
 }
 
-export function part2(input: string) {}
+export function part2(input: string) {
+  
+  const values = input.split('\n');
+  
+  return values.reduce((total, value) => {
+    const digits = value.split(' | ')[1].split(' ');
+
+    const lengths = [2, 3, 4, 7];
+    let sum = 0;
+
+    digits.forEach((digit) => {
+      if (lengths.includes(digit.length)) {
+        sum++;
+      }
+    });
+
+    return total + sum;
+  }, 0);
+}
