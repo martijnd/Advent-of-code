8b7dcb3c23205cf3b17c76553ab999218b924a4a
Martijn Dorsman <dorsman@mytaxflow.dev>
2021-12-08 17:30:35 +0100

    Solve day 8 part 1

---
+++ 2021/Day8/index.test.ts
@@ -0,0 +1,25 @@
+import { getInput } from '../../utils/getInput'
+import { part1, part2 } from './index'
+
+const data = getInput(__dirname)
+
+const exampleData = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`
+
+// test('2021 - Day 8: part 1', () => {
+//   expect(part1(exampleData)).toBe(26)
+//   expect(part1(data)).toBe(390)
+// })
+
+test('2021 - Day 8: part 2', () => {
+  expect(part2(exampleData)).toBe(61229)
+  // expect(part2(data)).toBe(undefined)
+})
