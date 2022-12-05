f4c75a6af065ef59074a7da0e498da370b1966b6
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-12-05 11:43:05 +0100

    Cleanup

--- 2022/Day5/index.ts
+++ 2022/Day5/index.ts
@@ -1,54 +1,52 @@
 export function part1(input: string) {
-  const [A, B] = input.split('\n\n');
-  const rows = A.split('\n');
-  const instructions = B.split('\n');
-  const numberOfCols = Number(rows.at(-1)?.trim().at(-1));
+  const [state, instructions] = parse(input);
 
-  let collection = Array.from(Array(numberOfCols), () => [] as string[]);
-  for (let colIndex = 0; colIndex <= numberOfCols - 1; colIndex++) {
-    rows.slice(0, -1).forEach((row) => {
-      const char = row[1 + colIndex * 4];
-      if (char !== ' ') {
-        collection[colIndex].push(char);
-      }
-    });
-  }
-
-  for (const instruction of instructions) {
+  instructions.forEach((instruction) => {
     const { amount, from, to } = parseInstructions(instruction);
     for (let i = 0; i < amount; i++) {
-      const moving = collection[from - 1].shift()!;
-      collection[to - 1] = [moving, ...collection[to - 1]];
+      const moving = state[from - 1].shift()!;
+      state[to - 1] = [moving, ...state[to - 1]];
     }
+  });
+  return state.reduce((total, [head]) => total + head, '');
+}
+
+function parse(input: string): [string[][], string[]] {
+  const [rows, instructions] = input
+    .split('\n\n')
+    .map((data) => data.split('\n'));
+  const numberOfCols = Number(rows.at(-1)?.trim().at(-1));
+
+  const collection = Array.from(Array(numberOfCols), () => [] as string[]);
+  for (let colIndex = 0; colIndex <= numberOfCols - 1; colIndex++) {
+    rows
+      // Skip last
+      .slice(0, -1)
+      .forEach((row) => {
+        const index = 1 + colIndex * 4;
+        const char = row[index];
+        if (char !== ' ') {
+          collection[colIndex] = [...collection[colIndex], char];
+        }
+      });
   }
-  return collection.reduce((acc, col) => acc + col[0], '');
+
+  return [collection, instructions];
 }
 
 function parseInstructions(instruction: string) {
-  const [, amount, , from, , to] = instruction.split(' ');
-  return { amount: Number(amount), from: Number(from), to: Number(to) };
+  const [, amount, , from, , to] = instruction.split(' ').map(Number);
+  return { amount, from, to };
 }
 
 export function part2(input: string) {
-  const [A, B] = input.split('\n\n');
-  const rows = A.split('\n');
-  const instructions = B.split('\n');
-  const numberOfCols = Number(rows.at(-1)?.trim().at(-1));
+  const [state, instructions] = parse(input);
 
-  let collection = Array.from(Array(numberOfCols), () => [] as string[]);
-  for (let colIndex = 0; colIndex <= numberOfCols - 1; colIndex++) {
-    rows.slice(0, -1).forEach((row) => {
-      const char = row[1 + colIndex * 4];
-      if (char !== ' ') {
-        collection[colIndex].push(char);
-      }
-    });
-  }
-
-  for (const instruction of instructions) {
+  instructions.forEach((instruction) => {
     const { amount, from, to } = parseInstructions(instruction);
-    const moving = collection[from - 1].splice(0, amount);
-    collection[to - 1] = [...moving, ...collection[to - 1]];
-  }
-  return collection.reduce((acc, col) => acc + col[0], '');
+    const moving = state[from - 1].splice(0, amount);
+    state[to - 1] = [...moving, ...state[to - 1]];
+  });
+
+  return state.reduce((total, [head]) => total + head, '');
 }
