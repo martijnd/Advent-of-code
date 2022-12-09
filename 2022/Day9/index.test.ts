231a131184622660003ee7736f32b67265582943
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-12-09 11:14:38 +0100

    wip

--- 2022/Day9/index.test.ts
+++ 2022/Day9/index.test.ts
@@ -12,12 +12,21 @@ D 1
 L 5
 R 2`;
 
+const exampleData2 = `R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`;
+
 test('2022 - Day 9: part 1', () => {
   expect(part1(exampleData)).toBe(13);
   expect(part1(data)).toBe(6642);
 });
 
-// test('2022 - Day 9: part 2', () => {
-//   expect(part2(exampleData)).toBe(undefined)
-//   // expect(part2(data)).toBe(undefined)
-// })
+test('2022 - Day 9: part 2', () => {
+  expect(part2(exampleData)).toBe(36);
+  // expect(part2(data)).toBe(undefined)
+});
--- 2022/Day9/index.ts
+++ 2022/Day9/index.ts
@@ -49,4 +49,54 @@ export function part1(input: string) {
   return visitedPositions.length;
 }
 
-export function part2(input: string) {}
+export function part2(input: string) {
+  const steps = input
+    .split('\n')
+    .map(
+      (step) =>
+        [step.split(' ')[0] as Direction, Number(step.split(' ')[1])] as const
+    );
+  const visitedPositions: { x: number; y: number }[] = [{ x: 0, y: 0 }];
+  // Starting point
+  let currentTailPositions = Array.from({ length: 10 }, () => ({ x: 0, y: 0 }));
+
+  const mutations: Record<Direction, { x: number; y: number }> = {
+    R: { x: 1, y: 0 },
+    L: { x: -1, y: 0 },
+    U: { x: 0, y: -1 },
+    D: { x: 0, y: 1 },
+  };
+
+  steps.forEach(([direction, amount]) => {
+    for (let i = 0; i < amount; i++) {
+      currentTailPositions.forEach((currentTailPosition, index) => {
+        if (index === 0) {
+          currentTailPositions[index] = {
+            x: currentTailPosition.x + mutations[direction].x,
+            y: currentTailPosition.y + mutations[direction].y,
+          };
+          return;
+        }
+        const previousPos = { ...currentTailPositions[index - 1] };
+        // Are they touching?
+        if (
+          Math.abs(previousPos.x - currentTailPosition.x) <= 1 &&
+          Math.abs(previousPos.y - currentTailPosition.y) <= 1
+        ) {
+          return;
+        }
+
+        currentTailPosition = previousPos;
+        if (
+          index === 9 &&
+          !visitedPositions.find(
+            ({ x, y }) => previousPos.x === x && previousPos.y === y
+          )
+        ) {
+          visitedPositions.push(previousPos);
+        }
+      });
+    }
+  });
+  return visitedPositions.length;
+}
