ff0fc7fb9b86abf3ddfd685a2055afbd891876bd
Martijn Dorsman <martijn.dorsman@gmail.com>
2023-12-01 11:49:04 +0100

    2023 day 1 part 2

--- 2023/Day1/index.test.ts
+++ 2023/Day1/index.test.ts
@@ -13,4 +13,15 @@ test('2023 - Day 1: part 1', () => {
   expect(part1(data)).toBe(56465);
 });
 
-test('2023 - Day 1: part 2', () => {});
+const exampleData2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
+test('2023 - Day 1: part 2', () => {
+  expect(part2(exampleData2)).toBe(281);
+  expect(part2(data)).toBe(55902);
+});
--- 2023/Day1/index.ts
+++ 2023/Day1/index.ts
@@ -17,3 +17,50 @@ export function part1(data: string) {
     return acc + Number(sum);
   }, 0);
 }
+
+export function part2(data: string) {
+  const map = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+  } as Record<string, number>;
+
+  const matches = Object.keys(map).map(String);
+
+  return data.split('\n').reduce((acc, line) => {
+    let lowest = Infinity;
+    let word = '';
+    matches.forEach((match) => {
+      const index = line.indexOf(match);
+      if (index >= 0 && index < lowest) {
+        lowest = index;
+        word = match;
+      }
+    });
+    let highest = -1;
+    let word2 = '';
+    matches.forEach((match) => {
+      const index = line.lastIndexOf(match);
+      if (index >= 0 && index > highest) {
+        highest = index;
+        word2 = match;
+      }
+    });
+    return acc + Number(map[word].toString() + map[word2].toString());
+  }, 0);
+}
