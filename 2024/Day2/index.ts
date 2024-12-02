7017f411a0967e673c97e1adfde12cf80be0819a
Martijn Dorsman <martijn.dorsman@gmail.com>
2024-12-02 10:50:14 +0100

    refactor: update Vite configuration and improve Day 2 solutions

--- 2024/Day2/index.ts
+++ 2024/Day2/index.ts
@@ -1,27 +1,23 @@
 export function part1(input: string) {
-  const levels = input.split('\n').map((line) => line.split(' ').map(Number));
-
-  return levels.reduce((acc, level) => {
-    return checkLevel(level) ? acc + 1 : acc;
-  }, 0);
+  return parse(input).reduce(
+    (acc, level) => (checkLevel(level) ? acc + 1 : acc),
+    0
+  );
 }
 
 export function part2(input: string) {
-  const levels = input.split('\n').map((line) => line.split(' ').map(Number));
+  return parse(input).reduce(
+    (acc, level) =>
+      checkLevel(level) ||
+      level.some((_, i) => checkLevel(level.toSpliced(i, 1)))
+        ? acc + 1
+        : acc,
+    0
+  );
+}
 
-  return levels.reduce((acc, level) => {
-    if (checkLevel(level)) {
-      return acc + 1;
-    }
-    const original = [...level];
-    const result = level.some((_, i) => {
-      level = [...original];
-      level.splice(i, 1);
-      return checkLevel(level);
-    });
-
-    return result ? acc + 1 : acc;
-  }, 0);
+function parse(input: string) {
+  return input.split('\n').map((line) => line.split(' ').map(Number));
 }
 
 function checkLevel(level: number[]) {
@@ -36,7 +32,7 @@ function checkLevel(level: number[]) {
   const smallDiff = level.every((num, i, arr) => {
     if (i === arr.length - 1) return true;
     const diff = Math.abs(num - arr[i + 1]);
-    return diff > 0 && diff <= 3;
+    return diff >= 1 && diff <= 3;
   });
 
   return (allDecreasing || allIncreasing) && smallDiff;
