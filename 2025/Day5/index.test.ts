26028561f685099701bb4cbb94c017346f22b1c7
Martijn Dorsman <martijn.dorsman@gmail.com>
2025-12-05 15:40:44 +0100

    Implement logic for part 2 of Day 5, 2025, including range merging and update tests with expected outputs.

--- 2025/Day5/index.test.ts
+++ 2025/Day5/index.test.ts
@@ -20,7 +20,7 @@ test('2025 - Day 5: part 1', () => {
   expect(part1(data)).toBe(505);
 });
 
-// test('2025 - Day 5: part 2', () => {
-//   expect(part2(exampleData)).toBe(undefined)
-//   // expect(part2(data)).toBe(undefined)
-// })
+test('2025 - Day 5: part 2', () => {
+  expect(part2(exampleData)).toBe(14);
+  expect(part2(data)).toBe(344423158480189);
+});
--- 2025/Day5/index.ts
+++ 2025/Day5/index.ts
@@ -10,4 +10,28 @@ export function part1(input: string) {
     }, 0);
 }
 
-export function part2(input: string) {}
+export function part2(input: string) {
+  const [rangesList] = input.split('\n\n');
+  const ranges = rangesList
+    .split('\n')
+    .map((r) => r.split('-').map(Number))
+    .sort((a, b) => a[0] - b[0]) as [number, number][];
+
+  const [initial, ...rest] = ranges;
+  const list: [number, number][] = [initial];
+
+  // iterate through remaining ranges
+  rest.forEach(([rangeStart, rangeEnd]) => {
+    // for each range:
+    // if it overlaps with current merged range, extend the current range
+    const [storedStart, storedEnd] = list[list.length - 1];
+    if (rangeStart <= storedEnd)
+      // make sure to take the highest highest end value
+      list[list.length - 1] = [storedStart, Math.max(storedEnd, rangeEnd)];
+    else {
+      list.push([rangeStart, rangeEnd]);
+    }
+  });
+
+  return list.reduce((total, [start, end]) => total + end - start + 1, 0);
+}
